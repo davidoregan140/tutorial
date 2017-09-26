fbb01fdd04be38179ae2d1544c31cfbb3ba28786
davidoregan140 <davidoregan140@gmail.com>
2017-09-26 22:31:32 +0100

    made some speculative changes

--- src/power.py
+++ src/power.py
@@ -3,17 +3,21 @@
 """
 
 def to_the_power(x, y = 2):
-    result = x
-    for i in range(0, y):
-        result = result * x
-    return result
 
-x = 9
+#trying this out:    
+    
+    return x ** y;
+    
+#    result = x
+#   for i in range(0, y):
+#        result = result * x
+#    return result
+
+x = 9 #
 
 y = 8 #
 
 
 
 print("{0} to the power of {1} is: {2}".format(x, y, to_the_power(x, y))
-
-      print("the end")
+print("The End")
